11e7aa86e3323640d6ad698cb8eae54287b546bb
王茂文 <3277954078@qq.com>
2020-05-14 08:45:46 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    'indent': 'off',
+    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
+    'space-before-function-paren': ['error', { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' }]
   }
 }
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// 配置文件路径别名
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+module.exports = {
+    lintOnSave: true,
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('api', resolve('src/api'))
+            .set('assets', resolve('src/assets'))
+            .set('base', resolve('src/base'))
+            .set('components', resolve('src/components'))
+            .set('pages', resolve('src/pages'));
+        // 格式.set('', resolve(''))
+    }
+};
